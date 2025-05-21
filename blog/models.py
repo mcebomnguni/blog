72ffac3a9c590007eaf50d7b8e88c3b5ca1c214f
mcebomnguni <marymnguni1000@gmail.com>
2025-05-21 18:27:00 +0200

    Add docstrings to models.py

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 class Post(models.Model):
+    """
+    A blog post model representing individual entries in the blog.
+
+    Attributes:
+        title (CharField): The title of the blog post (max 140 characters).
+        body (TextField): The main content of the blog post.
+        date (DateTimeField): The date and time the post was created (auto-set on creation).
+        signature (CharField): A short quote or signature line (default provided).
+    """
     title = models.CharField(max_length=140)  
     body = models.TextField()  
     date = models.DateTimeField(auto_now_add=True)  
